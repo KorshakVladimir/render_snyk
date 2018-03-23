0ca43a8c1ee7d3f7b11eda7d7abf6f7d1efd3fe4
KorshakVladimir <v.korshak.synergetica@gmail.com>
2018-03-23 20:24:14 +0200

    nginx uwsgi  0.1

--- frontend/render-ai-app/src/main.ts
+++ frontend/render-ai-app/src/main.ts
@@ -1,24 +1,21 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { } from 'node';
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
 import { hmrBootstrap } from './hmr';
 
-if (environment.production) {
+if (process.env.NODE_ENV === 'production') {
   enableProdMode();
 }
 
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
-
-// if (environment.hmr) {
-if (module[ 'hot' ]) {
-  hmrBootstrap(module, bootstrap);
+if (process.env.NODE_ENV !== 'production') {
+  if (module[ 'hot' ]) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    bootstrap();
+  }
 } else {
-  console.error('HMR is not enabled for webpack-dev-server!');
-  console.log('Are you using the --hmr flag for ng serve?');
+  bootstrap();
 }
-// } else {
-//   bootstrap();
-// }
